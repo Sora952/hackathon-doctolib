85227e157524127fa03f9ac221db5adda312accd
Armin <arminit3@gmail.com>
2020-06-24 09:25:24 +0200

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Page1 from "./pages/Page1";
+import Page2 from "./pages/Page2";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hackathon Doctolib</h1>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Page1 />
+          </Route>
+          <Route path="/page2">
+            <Page2 />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
