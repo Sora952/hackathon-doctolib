79e6e63fccea09696c82a0784194bacaf6f096f0
Sora952 <sora952c16b10@gmail.com>
2020-06-24 12:05:14 +0200

    truc

---
+++ pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+    return (
+      <div>
+        <nav>
+          <Link to="/"><div className="logo-doctolib">Doctolib</div></Link>
+
+        </nav>
+        <div>t</div>
+      </div>
+    )
+};
+
+export default Home;
