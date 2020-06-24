33c74ebe2dab2104cc25d3ff6a3c4f924561579c
Rincevent42 <porvooc@gmail.com>
2020-06-24 17:58:48 +0200

    add firebase module

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,5 +1,5 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+import firebase from "firebase/app";
+import "firebase/firestore";
 
 var firebaseConfig = {
   apiKey: "AIzaSyD4TpfZuZGX52he5xPinF2ix6MvYrDhMHE",
