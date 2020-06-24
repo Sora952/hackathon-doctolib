400abc4c7378fa7f8111aa324c63dddadc0e711e
Rincevent42 <porvooc@gmail.com>
2020-06-24 17:55:28 +0200

    clean index.html and add components/firebase.js

---
+++ src/components/firebase.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyD4TpfZuZGX52he5xPinF2ix6MvYrDhMHE",
+  authDomain: "hackathon-doctolib.firebaseapp.com",
+  databaseURL: "https://hackathon-doctolib.firebaseio.com",
+  projectId: "hackathon-doctolib",
+  storageBucket: "hackathon-doctolib.appspot.com",
+  messagingSenderId: "939555623923",
+  appId: "1:939555623923:web:d241f324f49ff3ae37f886",
+  measurementId: "G-TE29ERW19X",
+};
