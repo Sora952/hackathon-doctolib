aee1d1ee77ca6b6bf2ebc77b257ad1f5f3d98ba5
Armin <arminit3@gmail.com>
2020-06-25 02:55:41 +0200

    add Medicament file

---
+++ src/pages/Medicament.js
@@ -0,0 +1,10 @@
+import React from "react";
+import AddButton from "../components/addButton";
+
+export default function Medicament() {
+  return (
+    <div>
+      <AddButton />
+    </div>
+  );
+}
