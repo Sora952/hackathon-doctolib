02218b142ca6454fd8aecaf512c558f3c8b04fed
florenthoudeille <houdeille.florent@hotmail.fr>
2020-06-26 00:54:09 +0200

    Add images for button list on Patient page. Stylization of button on Patient Page

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -6,10 +6,18 @@ import ListItemText from "@material-ui/core/ListItemText";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import Avatar from "@material-ui/core/Avatar";
 import img4 from "../images/img4.png";
+import img5 from "../images/img5.png";
+import img6 from "../images/img6.png";
+import img7 from "../images/img7.png";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
+    // alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    width: "100px",
+    margin: "auto",
     "& > *": {
       margin: theme.spacing(1),
     },
@@ -28,26 +36,37 @@ export default function ButtonList() {
       <ListItem>
         <ListItemAvatar>
           <Avatar className={classes.large}>
-            <img src={img4} />
+            <img src={img4} alt="Medication"/>
           </Avatar>
         </ListItemAvatar>
-        <ListItemText primary=" Médicaments" />
+        <ListItemText primary="Medication" />
       </ListItem>
+
       <ListItem>
         <ListItemAvatar>
           <Avatar className={classes.large}>
-            <img src={img4} />
+            <img src={img5} alt="Analysis"/>
           </Avatar>
         </ListItemAvatar>
-        <ListItemText primary=" Médicaments" />
+        <ListItemText primary="My analysis" />
       </ListItem>
+
       <ListItem>
         <ListItemAvatar>
           <Avatar className={classes.large}>
-            <img src={img4} />
+            <img src={img6} alt="Doctor"/>
           </Avatar>
         </ListItemAvatar>
-        <ListItemText primary=" Médicaments" />
+        <ListItemText primary="My doctors" />
+      </ListItem>
+
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar className={classes.large}>
+            <img src={img7} alt="Appointement"/>
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary="Appointement" />
       </ListItem>
     </List>
   );
