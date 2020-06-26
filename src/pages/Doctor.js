e37861df1268b47d82a79a1c3a956dee63a56478
Armin <arminit3@gmail.com>
2020-06-26 04:41:52 +0200

    add doctor page

--- src/pages/Doctor.js
+++ src/pages/Doctor.js
@@ -1,32 +1,93 @@
 import React from "react";
-import withStyles from "@material-ui/core/styles/withStyles";
-import firebase from "../firebase";
-import { withRouter } from "react-router-dom";
+import "../styles/Doctor.css";
+import ListItems from "../components/ListItems";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import AddButton from "../components/addButton";
 
-const styles = (theme) => ({
-  main: {
-    width: "auto",
-    display: "block", // Fix IE 11 issue.
-    marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
-    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
-      width: 400,
-      marginLeft: "auto",
-      marginRight: "auto",
-    },
-  },
-});
+library.add(faTrash);
 
-function Doctor(props) {
-  const { classes } = props;
-
-  if (!firebase.getCurrentUsername()) {
-    alert("Please login first");
-    props.history.replace("/loginD");
-    return null;
+class Doctor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      currentItem: {
+        text: "",
+        key: "",
+      },
+    };
+    this.addItem = this.addItem.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+    this.setUpdate = this.setUpdate.bind(this);
   }
-
-  return <main className={classes.main}>Page gestion des patients</main>;
+  addItem(e) {
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    if (newItem.text !== "") {
+      const items = [...this.state.items, newItem];
+      this.setState({
+        items: items,
+        currentItem: {
+          text: "",
+          key: "",
+        },
+      });
+    }
+  }
+  handleInput(e) {
+    this.setState({
+      currentItem: {
+        text: e.target.value,
+        key: Date.now(),
+      },
+    });
+  }
+  deleteItem(key) {
+    const filteredItems = this.state.items.filter((item) => item.key !== key);
+    this.setState({
+      items: filteredItems,
+    });
+  }
+  setUpdate(text, key) {
+    console.log("items:" + this.state.items);
+    const items = this.state.items;
+    items.map((item) => {
+      if (item.key === key) {
+        console.log(item.key + "    " + key);
+        item.text = text;
+      }
+    });
+    this.setState({
+      items: items,
+    });
+  }
+  render() {
+    return (
+      <div>
+        <header>
+          <p>{this.state.items.text}</p>
+          <ListItems
+            items={this.state.items}
+            deleteItem={this.deleteItem}
+            setUpdate={this.setUpdate}
+          />
+          <form id="to-do-form" onSubmit={this.addItem}>
+            <input
+              type="text"
+              placeholder="Enter Medicament"
+              value={this.state.currentItem.text}
+              onChange={this.handleInput}
+            />
+            <button>
+              <AddButton type="submit" />
+            </button>
+          </form>
+        </header>
+      </div>
+    );
+  }
 }
 
-export default withRouter(withStyles(styles)(Doctor));
+export default Doctor;
