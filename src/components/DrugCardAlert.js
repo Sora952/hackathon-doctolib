82b6fee322464705b70321ff59187545d01aa6fc
Armin <arminit3@gmail.com>
2020-06-26 02:35:22 +0200

    update alert list

--- src/components/DrugCardAlert.js
+++ src/components/DrugCardAlert.js
@@ -1,15 +1,11 @@
 import React from "react";
-// import moment from "moment";
 import "../styles/CardAlert.css";
 
 const DrugCardAlert = ({ alert, deletedDrugs }) => {
-  const drugs = alert;
-  console.log(drugs);
-  const prescriptionDate = drugs.hours;
   return (
     <div className="CardAlert">
       <ul className="globalAlerts">
-        {drugs.map((oneDrug) => (
+        {alert.map((oneDrug) => (
           <div key={oneDrug.id}>
             <li className="list-alert">
               <div className="HeaderCard">
@@ -18,15 +14,12 @@ const DrugCardAlert = ({ alert, deletedDrugs }) => {
                   {oneDrug.dosage} {oneDrug.method}
                 </h3>
               </div>
-              {/* <div className="timeRemaining">
-                <p>
-                  {moment().endOf(prescriptionDate).fromNow()} h left before
-                  taking it.
-                </p>
-              </div> */}
+              <div className="timeRemaining">
+                <p>Scheduled for 8:00 AM, today, June 26</p>
+              </div>
               <div className="card-button">
                 <button className="first" type="button">
-                  Don't take
+                  Ignore
                 </button>
                 <button className="second" type="button">
                   Remind me
